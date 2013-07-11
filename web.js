4f58f61f4e367e7802022d33173beea72ae1bca1
John Doe <example@stanford.edu>
2013-07-11 07:53:55 +0000

    Changes to read from index.html

--- web.js
+++ web.js
@@ -3,8 +3,12 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  fs.readFile('intedex.html', function(err, data) {
+  if (err) throw err; 
+   response.send(data);
+  });
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
