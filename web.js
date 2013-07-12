927906712b24736d0387c8b7190a91bb65a4eb1b
John Doe <example@stanford.edu>
2013-07-12 05:03:43 +0000

    Final submitted version for HW3

--- web.js
+++ web.js
@@ -1,17 +1,12 @@
 var express = require('express');
-var fs = require ('fs');
-
+var fs = require('fs');
 var app = express.createServer(express.logger());
-
-buf = new Buffer(256); 
+buf = new Buffer(256);
 
 app.get('/', function(request, response) {
-  fs.readFile('index.html', function(err, data) {
-  if (err) throw err; 
-   response.send(buf.toString(data));
-  });
+    buf = fs.readFileSync('index.html');
+    response.send(buf.toString());
 });
-
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
